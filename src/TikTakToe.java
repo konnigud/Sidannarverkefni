344030406d3af0c6aa61f35edc39655c76671ca0
Konráð Guðmundsson <konnigud@gmail.com>
2012-11-14 21:04:54 +0000

    Project started
    Created an empty project called TikTakToe in intellij

---
+++ src/TikTakToe.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konni
+ * Date: 14.11.2012
+ * Time: 20:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class TikTakToe {
+}
