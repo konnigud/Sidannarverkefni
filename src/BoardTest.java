5f300d831362129687c86d5f0603ee80fb7a4821
Gudny <gudnyg11@ru.is>
2012-11-14 22:37:41 +0000

    Board and BoardTest classes created

---
+++ src/BoardTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guðný
+ * Date: 11/14/12
+ * Time: 10:28 PM
+ *
+ * @author Guðný Björk Gunnarsdóttir
+ */
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+public class BoardTest extends TestCase {
+}
