3b8c952859051f3a5d212e7d1350e56a847a335f
OskarOgri <snepill@gmail.com>
2012-11-14 23:03:14 +0000

    Class and interface setup for ComputerAI interface and AI class

---
+++ src/AI.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Óskar Ögri Birgisson
+ * Date: 14.11.2012
+ * Last Updated: 14.11.2012
+ * Time: 23:00
+ * Description:
+ */
+public class AI implements ComputerAI {
+    @Override
+    public int solve(Board board) {
+        return 0;
+    }
+}
