596e0f0dec306cd5de485bf70f44d59d7bb3e9ad
arnar <arnaru09@gmail.com>
2012-11-14 22:42:43 +0000

    Square and SquareTest Classes created..

---
+++ src/Square.java
@@ -0,0 +1,47 @@
+import sun.plugin2.message.MarkTaintedMessage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Arnar Thor
+ * Date: 14.11.2012
+ * Time: 22:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class Square {
+
+    private Mark mark;
+    private int index;
+
+    public boolean isEmpty(){
+        if(this.mark == Mark.Open)
+            return true;
+        return false;
+    }
+    public Square(){
+        this.mark = 0;
+        this.index = 0;
+    }
+
+    public Board clone(){
+        Square s = this.MemberwiseClone() as Square;
+        s.Mark = new Mark();
+        s.Mark = this.mark;
+        return s;
+    }
+
+    public Mark getMark() {
+        return mark;
+    }
+
+    public void setMark(Mark mark) {
+        this.mark = mark;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
---
+++ src/SquareTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Arnar Thor
+ * Date: 14.11.2012
+ * Time: 22:34
+ * To change this template use File | Settings | File Templates.
+ */
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+public class SquareTest extends TestCase {
+
+}
