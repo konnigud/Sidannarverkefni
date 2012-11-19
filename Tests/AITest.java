dac23e93d3e2ca0d873e2f9f3b2e9785c783125d
OskarOgri <snepill@gmail.com>
2012-11-19 17:20:41 +0000

    AI and test finished...

--- Tests/AITest.java
+++ Tests/AITest.java
@@ -1,32 +1,24 @@
-import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
 * Created with IntelliJ IDEA.
 * Author: Óskar Ögri Birgisson
 * Date: 19.11.2012
 * Last Updated: 19.11.2012
-* Time: 13:52
+* Time: 17:06
 * Description:
 */
 public class AITest {
-// Member Variables:
-
-// Enums:
-
-// Constructors:
-
-// Functions:
-
-// Getters / Setters
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
 
     @Test
     public void testSolve() throws Exception {
+
+        ComputerAI testAI = new AI();
+        Board testBoard = new Board();
+        testBoard.getSquares()[4].setMark(Mark.X);
+
+        assertEquals("Error 1! AI move not what it's supposed to be!", 0, testAI.solve(testBoard));
 
     }
 }
--- src/AI.java
+++ src/AI.java
@@ -9,6 +9,82 @@
 public class AI implements ComputerAI {
     @Override
     public int solve(Board board) {
-        return 0;
+        int Omove = -1;
+        int v = -2;
+        int alpha = -2;
+        int beta = 2;
+
+        for (int i : board.GetValidMoves())
+        {
+            Board successor = (Board)board.clone();
+            successor.getSquares()[i].setMark(Mark.O);
+            int vTemp = Min_Value(successor, alpha, beta);
+            // take max of min values
+            if (vTemp > v)
+            {
+                Omove = i;
+                v = vTemp;
+            }
+            if (v >= beta)
+                break;
+            alpha = Max(alpha, v);
+        }
+
+        return Omove;
+
+    }
+
+    private int Max_Value(Board b, int alpha, int beta)
+    {
+        if (b.Evaluate(Mark.X))
+            return -1; // minimize
+        if (b.Evaluate(Mark.O))
+            return 1;  // maximize
+        if (b.IsFull())
+            return 0;
+
+        int v = -2;
+        for (int i : b.GetValidMoves())
+        {
+            Board s = (Board)b.clone(); // successor
+            s.getSquares()[i].setMark(Mark.O);
+            v = Max(v, Min_Value(s, alpha, beta));
+            if (v >= beta)
+                return v;
+            alpha = Max(alpha, v);
+        }
+        return v;
+    }
+
+    private int Min_Value(Board b, int alpha, int beta)
+    {
+        if (b.Evaluate(Mark.X))
+            return -1;  // minimize
+        if (b.Evaluate(Mark.O))
+            return 1;   // maximize
+        if (b.IsFull())
+            return 0;
+
+        int v = 2;
+        for (int i : b.GetValidMoves())
+        {
+            Board s = (Board)b.clone(); //successor
+            s.getSquares()[i].setMark(Mark.X);
+            v = Min(v, Max_Value(s, alpha, beta));
+            if (v <= alpha)
+                return v;
+            beta = Min(beta, v);
+        }
+        return v;
+    }
+
+    private int Max(int a, int b)
+    {
+        return (a > b ? a : b);
+    }
+
+    private int Min(int a, int b)
+    {
+        return (a < b ? a : b);
     }
 }
